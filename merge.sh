910f641cd91c6bfc579d12f4c767b716c96d7b72
CiccioTecchio <fr.vicidomini94@gmail.com>
2020-02-17 14:06:30 +0100

    edited

--- merge.sh
+++ merge.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-python3 mondiale_2018.csv ds.csv 6
-python3 auspol2019.csv ds.csv 2
-python3 dem_vs_rep ds.csv 2
-python3 troll_tweet ds.csv 7
-python3 trumptweets.csv ds.csv 2
-python3 gender-classifier-DFE-791531.csv ds.csv 19
-python3 GBcomments.csv ds.csv 1
-python3 UScomments.csv ds.csv 1
-python3 airlineTweet.csv ds.csv 10
-python3 amazon_job ds.csv ds.csv 4 #description
-python3 amazon_job ds.csv ds.csv 5 #basic qualification
-python3 amazon_job ds.csv ds.csv 6 #preferred qualification
+#python3 Main.py mondiale.csv ds.csv 6
+#python3 Main.py election_day_tweets_US.csv ds.csv 0
+#python3 Main.py TweetsAustralianElection2019.csv ds.csv 2
+#python3 Main.py dem_vs_rep.csv ds.csv 2
+#python3 Main.py Russian_Troll_Tweets.csv ds.csv 7
+#python3 Main.py trumptweets.csv ds.csv 2
+python3 Main.py gender-classifier-DFE-791531.csv ds.csv 19
+python3 Main.py GBcomments.csv ds.csv 1
+python3 Main.py UScomments.csv ds.csv 1
+#python3 Main.py Oscar2019Tweets.csv ds.csv 3
+#python3 Main.py Tweets_US_Airline.csv ds.csv 10
+python3 Main.py amazon_jobs_dataset.csv ds.csv 4 #description
+python3 Main.py amazon_jobs_dataset.csv ds.csv 5 #basic qualification
+python3 Main.py amazon_jobs_dataset.csv ds.csv 6 #preferred qualification
+python3 Main.py SMS_spam.csv ds.csv 1
